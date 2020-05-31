9c8d7d9ca24b66b2a2a7a9bc701f102e29168b84
Vinita V Nayak <vinitha.nayak1994@gmail.com>
2020-05-31 21:56:27 +0530

    second commit

--- javap.java
+++ javap.java
@@ -1,5 +1,8 @@
 public class javap{
   public static void main(String args[]){
+	  int a=10,b=20;
   System.out.println("Hello java");
+  int c=a+b;
+  System.out.println("Sum of 2 numbers is "+c);
   }
 }
